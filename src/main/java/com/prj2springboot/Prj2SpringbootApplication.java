8c72da667e61917a5eb7bb2c598fcb8c6b717647
sunghwan <simcom0820@naver.com>
2024-05-21 10:13:12 +0900

    최초커밋

---
+++ src/main/java/com/prj2springboot/Prj2SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.prj2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2SpringbootApplication.class, args);
+    }
+
+}
